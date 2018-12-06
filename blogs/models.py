ae0c831db7a59b56257cc57a9106e4bb43f03b3c
jefffin <jeffin@jusfocus.com>
2018-12-06 13:53:17 +0530

    blog page model added and migrated

--- blogs/models.py
+++ blogs/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class BlogPage(models.Model):
+    """The model to save a blog page. 
+    The body is saved as markdown. tags are implemented via taggit. authors is form users. 
+    subtitle is the plain text 2 sentence description about the blog.
+    """
+    title = models.CharField(max_length=300)
+    subtitle = models.TextField()
+    body = models.TextField()
+
+    def __str__(self):
+        return self.title
